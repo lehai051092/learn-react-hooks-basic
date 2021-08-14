f27d4d73b1d6d7d5b4a3dc6c5f354fd324979b82
hailt-2586 <le.thanh.hai@sun-asterisk.com>
2021-08-14 14:09:25 +0700

    useEffect() lab - Tính năng phân trang

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {useEffect, useState} from "react";
 import TodoList from "./components/TodoList";
 import TodoForm from "./components/TodoForm";
 import PostList from "./components/PostList";
+import Pagination from "./components/Pagination";
+import queryString from 'query-string';
 
 function App() {
     const [todoList, setTodoList] = useState(
@@ -15,22 +17,34 @@ function App() {
     );
 
     const [postList, setPostList] = useState([]);
+    const [pagination, setPagination] = useState(
+        {
+            _page: 1,
+            _limit: 10,
+            _totalRows: 1
+        }
+    );
+    const [filters, setFilters] = useState({
+        _limit: 10,
+        _page: 1
+    });
 
     useEffect(() => {
         async function fetchPostList() {
             try {
-                const requestUrl = 'https://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1';
+                const paramsString = queryString.stringify(filters);
+                const requestUrl = `https://js-post-api.herokuapp.com/api/posts?${paramsString}`;
                 const response = await fetch(requestUrl);
                 const responseJSON = await response.json();
-                const {data} = responseJSON;
+                const {data, pagination} = responseJSON;
                 setPostList(data);
+                setPagination(pagination);
             } catch (e) {
                 console.log(e.message);
             }
         }
-
         fetchPostList();
-    }, []);
+    }, [filters]);
 
     const handleTodoClick = (todo) => {
         const index = todoList.findIndex(x => x.id === todo.id);
@@ -50,6 +64,14 @@ function App() {
         setTodoList(newTodoList);
     }
 
+    const handlePageChane = (newPage) => {
+        console.log(newPage);
+        setFilters({
+            ...filters,
+            _page: newPage
+        });
+    }
+
     return (
         <div className="app">
             <h1>Welcome to React Hooks!</h1>
@@ -65,6 +87,7 @@ function App() {
             <hr/>
             <h2>Post List</h2>
             <PostList postList={postList}/>
+            <Pagination pagination={pagination} onPageChange={handlePageChane}/>
         </div>
     );
 }
